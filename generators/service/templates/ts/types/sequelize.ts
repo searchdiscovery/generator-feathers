651f0db5ce9120a45870750db2d1c53cb76d67e6
Nate <nathan.jackson@searchdiscovery.com>
2020-05-06 14:40:07 -0400

    🎨 Updated type in class generator

--- generators/service/templates/ts/types/sequelize.ts
+++ generators/service/templates/ts/types/sequelize.ts
@@ -2,7 +2,7 @@ import { Service, SequelizeServiceOptions } from 'feathers-sequelize';
 import { Application } from '<%= relativeRoot %>declarations';
 
 class <%= serviceClassName %> extends Service {
-  constructor(options: object, app: Application) {
+  constructor(options: Partial<SequelizeServiceOptions>, app: Application) {
     super(options);
   }
 }
