f9af2cd84f930b1ef6110ee16698965c53543f66
Nate <nathan.jackson@searchdiscovery.com>
2020-05-11 21:08:24 -0400

    🐛 Fixed test template

--- generators/service/templates/ts/test.mocha.ts
+++ generators/service/templates/ts/test.mocha.ts
@@ -3,7 +3,7 @@ import 'mocha';
 
 const { app, expect } = global;
 
-describe(`\'<%= name %>\' service`, () => {
+describe(`'<%= name %>' service`, () => {
   it('registered the service', () => {
     const service = app.service('<%= path %>');
     expect(service).to.be.ok;
